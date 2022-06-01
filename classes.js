fd832873f0c6ff4c469b76f5d14888ff032534f5
Chantal Goethals <chantalgoethals@gmail.com>
2022-06-01 15:53:19 -0600

    Feat: add randomizer

--- classes.js
+++ classes.js
@@ -1,7 +1,19 @@
+
+var newId = document.querySelector("#swatch-1")
+
+
 class Color {
-  constructor() {
-    this.color = 'color';
+  constructor(hexId) {
+    this.color = genColor();
     this.locked =false;
+  }
+
+   genColor(combos){
+    var hexId = ``;
+    var combos = `abcdef0123456789`;
+  for(var i = 0; i < 6; i++)
+    hexId += combos.charAt(Math.floor(Math.random() * combos.length));
+  return newId = `#${hexId}`;
   }
 };
 
