8b2d93fca1e5cc980a4a53992b971492d695342c
jmedina <ndgns2@gmail.com>
2022-06-02 11:26:42 -0600

    Feature: add Palette method

--- src/Palette.js
+++ src/Palette.js
@@ -1,10 +1,16 @@
-// var Color = require("./Color")
+var Color = require("./Color")
 
 class Palette {
     constructor() {
       this.colors = [];
       this.id = Date.now();
     }
+     addToPalette() {
+      var color = new Color.genColor()
+      if (this.colors < 5) {
+    this.colors.push(color)
+  
+      }
 }
 
     //   module.exports = Palette
