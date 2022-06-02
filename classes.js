b17ea381499d064a2d54370db4dc358119048c49
jmedina <ndgns2@gmail.com>
2022-06-01 19:42:34 -0600

    Docs: Add class files

--- classes.js
+++ classes.js
@@ -1,27 +1,22 @@
 
-var newId = document.querySelector("#swatch-1")
-
-
 class Color {
-  constructor(hexId) {
-    this.color = genColor();
+  constructor() {
+    this.color = '#ffffff';
     this.locked =false;
   }
-
-   genColor(combos){
+  genColor(){
     var hexId = ``;
     var combos = `abcdef0123456789`;
-  for(var i = 0; i < 6; i++)
+    for(var i = 0; i < 6; i++){
     hexId += combos.charAt(Math.floor(Math.random() * combos.length));
-  return newId = `#${hexId}`;
-  }
+    this.color = `#${hexId}`
+}
 };
+
+module.exports = Color
 
 
 class Palette {
   constructor() {
     this.colors = [];
     this.id = Date.now();
-  }
-
-}
