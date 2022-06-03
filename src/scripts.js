5fbad7eed5af8ea131d1354279d36b68dab968c7
jmedina <ndgns2@gmail.com>
2022-06-03 17:14:18 -0600

    Feature: Add save palette option

--- src/scripts.js
+++ src/scripts.js
@@ -1,7 +1,5 @@
-var savedPalettes = []
+var savedPalettes = [];
 var freshPalette;
-
-
 
 
 var label1 = document.querySelector('#hex-code-1')
@@ -14,22 +12,13 @@ var swatch2 = document.querySelector('#swatch-2')
 var swatch3 = document.querySelector('#swatch-3')
 var swatch4 = document.querySelector('#swatch-4')
 var swatch5 = document.querySelector('#swatch-5')
-var lock = document.querySelector('.locks')
-// var lock2 = document.querySelector('#lock-2')
-// var lock3 = document.querySelector('#lock-3')
-// var lock4 = document.querySelector('#lock-4')
-// var lock5 = document.querySelector('#lock-5')
 var randomButton = document.querySelector('#random')
 var savedButton = document.querySelector('#save')
-var swatchContainer = document.querySelector('.swatch')
+var swatchContainer = document.querySelector('.swatch-container')
 
-window.addEventListener('load', loadNewPalette)
-randomButton.addEventListener('click', randomPalette)
+window.addEventListener('load', makeNewPalette)
+randomButton.addEventListener('click', makeNewPalette)
 savedButton.addEventListener('click', savePalette)
-
-
-
-
 
 // lock.addEventListener('click', changeLock)
 
@@ -41,29 +30,28 @@ savedButton.addEventListener('click', savePalette)
 // function test(element){
 // element.classList.toggle('hidden')
 // }
-function loadNewPalette(freshPalette) {
-   freshPalette = new Palette();
-// for (var i = 0; i < freshPalette.colors.length; i++) {
+function makeNewPalette() {
+    freshPalette = new Palette();
+//  for (var i = 0; i < freshPalette.colours.length; i++) {
 // swatchContainer[i].style.backgroundColor = freshPalette.colors[i].color;
 // }
+  swatch1.style.backgroundColor = freshPalette.colors[0].color,
+  swatch2.style.backgroundColor = freshPalette.colors[1].color,
+  swatch3.style.backgroundColor = freshPalette.colors[2].color,
+  swatch4.style.backgroundColor = freshPalette.colors[3].color,
+  swatch5.style.backgroundColor = freshPalette.colors[4].color,
   label1.innerText = freshPalette.colors[0].color
   label2.innerText = freshPalette.colors[1].color
   label3.innerText = freshPalette.colors[2].color
   label4.innerText = freshPalette.colors[3].color
   label5.innerText = freshPalette.colors[4].color
-  swatch1.style.backgroundColor = freshPalette.colors[0].color
-  swatch2.style.backgroundColor = freshPalette.colors[1].color
-  swatch3.style.backgroundColor = freshPalette.colors[2].color
-  swatch4.style.backgroundColor = freshPalette.colors[3].color
-  swatch5.style.backgroundColor = freshPalette.colors[4].color
-
 }
 
 function savePalette() {
-    loadNewPalette()
-    savedPalettes.push(freshPalette)
-    console.log(savedPalettes)
-
+    if(!savedPalettes.includes(freshPalette)){
+        savedPalettes.push(freshPalette)
+        console.log(savedPalettes)
+    }
 }
 
 function randomPalette() {
@@ -89,3 +77,7 @@ function genColor(){
   }
     return `#${hexId}`
 }
+
+// for (var i = 0; i < freshPalette.colors.length; i++) {
+// swatchContainer[i].style.backgroundColor = freshPalette.colors[i].color;
+// }
