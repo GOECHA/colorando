c0c428f7630feb6805ee091438e8266b0f7f8b27
Chantal Goethals <chantalgoethals@gmail.com>
2022-06-04 17:57:02 -0600

    refractor: configure toggle lock

--- src/scripts.js
+++ src/scripts.js
@@ -8,78 +8,60 @@ var randomButton = document.querySelector('#random')
 var savedButton = document.querySelector('#save')
 var swatchContainer = document.querySelector('.swatch-container')
 var savedSection = document.querySelector('.saved-palettes')
-var lock1 = document.querySelector('#locked-1')
-var unlock1 = document.querySelector('#unlocked-1')
-var lock2 = document.querySelector('#locked-2')
-var unlock2 = document.querySelector('#unlocked-2')
-var lock3 = document.querySelector('#locked-3')
-var unlock3 = document.querySelector('#unlocked-3')
-var lock4 = document.querySelector('#locked-4')
-var unlock4 = document.querySelector('#unlocked-4')
-var lock5 = document.querySelector('#locked-5')
-var unlock5 = document.querySelector('#unlocked-5')
-var lock1Container = document.querySelector('#lock-1')
-var lock2Container = document.querySelector('#lock-2')
-var lock3Container = document.querySelector('#lock-3')
-var lock4Container = document.querySelector('#lock-4')
-var lock5Container = document.querySelector('#lock-5')
 var hidePalettes = document.querySelector('#hide-palettes')
 var savedSection = document.querySelector('.saved-palettes')
+var locks = document.querySelectorAll('.locks')
 
 
 window.addEventListener('load', makeNewPalette)
 randomButton.addEventListener('click', makeNewPalette)
 savedButton.addEventListener('click', savePalette)
-// hidePalettes.addEventListener('click', hideSavedPalettes)
-lock1Container.addEventListener('click', lockUnlock1)
-lock2Container.addEventListener('click', lockUnlock2)
-lock3Container.addEventListener('click', lockUnlock3)
-lock4Container.addEventListener('click', lockUnlock4)
-lock5Container.addEventListener('click', lockUnlock5)
-// unlock1.addEventListener('click', lockUnlock)
+swatchContainer.addEventListener('click', lockColor)
+// hidePalettes.addEventListener('click', savedSection)
 
-function lockUnlock1(){
-  lock1.classList.toggle('hidden');
-  unlock1.classList.toggle('hidden');
+
+
+function lockColor(e) {
+  console.log(e.target)
+
+  for (var i = 0; i < freshPalette.colors.length)
+  // lockHex.currentTarget
+// var hexId = swatches.dataset.id //not id, but an element within this.colors 
+  
+// for (var i = 0; i < freshPalette.colors.length; i ++){
+//    if (freshPalette[i].id == hexId){
+//      locks.classList.add('hidden')
+//    }
+// } 
+// console.log(`1`, swatches)
+
+
 }
-function lockUnlock2(){
-  lock2.classList.toggle('hidden')
-  unlock2.classList.toggle('hidden')
-}
-function lockUnlock3(){
-  lock3.classList.toggle('hidden')
-  unlock3.classList.toggle('hidden')
-}
-function lockUnlock4(){
-  lock4.classList.toggle('hidden')
-  unlock4.classList.toggle('hidden')
-}
-function lockUnlock5(){
-  lock5.classList.toggle('hidden')
-  unlock5.classList.toggle('hidden')
-}
+
+
 
 function makeNewPalette() {
     freshPalette = new Palette();
     for (var i = 0; i < freshPalette.colors.length; i++) {
       allHexCodes[i].innerText = freshPalette.colors[i].color
       allSwatches[i].style.backgroundColor = freshPalette.colors[i].color;
-    }
+    }locks.id = hexId
 }
 
 function savePalette() {
     savedSection.classList.remove('hidden')
-    if(!savedPalettes.includes(freshPalette)){
+    if(!savedPalettes.includes(freshPalette) && savedPalettes.length < 8){
         savedPalettes.push(freshPalette)
         console.log(savedPalettes)
-    }
-    append()
-    makeNewPalette()
+        append()
+        makeNewPalette()
+    }   
 }
+
 function append () {
   var testing = document.createElement('figure')
   testing.classList.add('mini-hex-container')
-  testing.setAttribute('id',`${freshPalette.id}`)
+  testing.lockColor('id',`${freshPalette.id}`)
   testing.innerHTML = `<div class="mini-hex mini-hex1" style="background: ${freshPalette.colors[0].color};"></div>
   <div class="mini-hex mini-hex2" style="background:${freshPalette.colors[1].color};"></div>
   <div class="mini-hex mini-hex3" style="background:${freshPalette.colors[2].color};"></div>
@@ -88,18 +70,6 @@ function append () {
   <img class="mini-hex" id="t-can" src="./assets/SVG/trash-can.svg" alt="t-can">`
   savedSection.appendChild(testing)
 }
-
-
-// function displaySaved() {
-//   savedSection.classList.remove('hidden')
-//   miniHex1.style.backgroundColor = savedPalettes[0].colors[0].color
-//   miniHex2.style.backgroundColor = savedPalettes[0].colors[1].color
-//   miniHex3.style.backgroundColor = savedPalettes[0].colors[2].color
-//   miniHex4.style.backgroundColor = savedPalettes[0].colors[3].color
-//   miniHex5.style.backgroundColor = savedPalettes[0].colors[4].color
-// }
-
-
 
 
 
@@ -114,8 +84,10 @@ function genColor(){
 }
 
 
+// function show(element){
+// element.classList.remove.("hidden")
+// }
 
-
-function hideSavedPalettes(){
-  savedSection.classList.add('hidden')
-}
+// function hide(element){
+//   element.classList.add.("hidden")
+//   }
